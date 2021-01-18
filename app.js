f1bf4eeffa203a320fd57ef82380edd4f61e7c21
mikidafi <mchl.str84@gmail.com>
2021-01-18 18:00:24 +0100

    Mensie upravy

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ function randomPcGuess() {
 }
 const result = randomPcGuess();
 
-const playerLength = player.length;
-const pcLength = result.length;
 
 if ( player == 'rock' && result == 'rock' ){
     console.log(`${player} = ${result}: Its a draw!`);
@@ -19,25 +17,25 @@ if ( player == 'rock' && result == 'rock' ){
 }else if ( player == 'scissors' && result == 'scissors' ){
     console.log(`${player} = ${result}: Its a draw!`);
 }
-
+// ******************************************************
 else if( player == 'rock' && result == 'paper' ) {
     console.log(`${player} < ${result}: Computer wins!`)
 }else if ( player == 'rock' && result == 'scissors' ){
     console.log(`${player} > ${result}: Player wins!`);
 }
-
+// ******************************************************
 else if ( player == 'paper' && result == 'rock' ){
     console.log(`${player} > ${result}: Player wins!`);
 }else if ( player == 'paper' && result == 'scissors' ){
     console.log(`${player} < ${result}: Computer wins!`);
 }
-
+// ******************************************************
 else if ( player == 'scissors' && result == 'rock' ){
     console.log(`${player} < ${result}: Computer wins!`);
 }else if ( player == 'scissors' && result == 'paper' ){
     console.log(`${player} > ${result}: Player wins!`);
 }
-
+// ******************************************************
 else{
     console.log("Enter rock, paper or scissors!")
 }

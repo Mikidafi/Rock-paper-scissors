bb682b3be96181bcac08256b9d7930957b18d33f
mikidafi <mchl.str84@gmail.com>
2021-01-18 22:20:24 +0100

    latest comit

--- app.js
+++ app.js
@@ -1,43 +1,40 @@
-const compGuesses = ['rock', 'paper', 'scissors'];
+const compGuesses = ["rock", "paper", "scissors"];
 const player = prompt();
 
 // Computer function
 function randomPcGuess() {
-    const randomIndex = Math.floor(Math.random(compGuesses) * compGuesses.length);
-    const item = compGuesses[randomIndex];
-    return item;
+  const randomIndex = Math.floor(Math.random(compGuesses) * compGuesses.length);
+  const item = compGuesses[randomIndex];
+  return item;
 }
 const result = randomPcGuess();
 
-
-if ( player == 'rock' && result == 'rock' ){
-    console.log(`${player} = ${result}: Its a draw!`);
-}else if ( player == 'paper' && result == 'paper' ){
-    console.log(`${player} = ${result}: Its a draw!`);
-}else if ( player == 'scissors' && result == 'scissors' ){
-    console.log(`${player} = ${result}: Its a draw!`);
+if (player == "rock" && result == "rock") {
+  console.log(`${player} = ${result}: Its a draw!`);
+} else if (player == "paper" && result == "paper") {
+  console.log(`${player} = ${result}: Its a draw!`);
+} else if (player == "scissors" && result == "scissors") {
+  console.log(`${player} = ${result}: Its a draw!`);
 }
 // ******************************************************
-else if( player == 'rock' && result == 'paper' ) {
-    console.log(`${player} < ${result}: Computer wins!`)
-}else if ( player == 'rock' && result == 'scissors' ){
-    console.log(`${player} > ${result}: Player wins!`);
+else if (player == "rock" && result == "paper") {
+  console.log(`${player} < ${result}: Computer wins!`);
+} else if (player == "rock" && result == "scissors") {
+  console.log(`${player} > ${result}: Player wins!`);
 }
 // ******************************************************
-else if ( player == 'paper' && result == 'rock' ){
-    console.log(`${player} > ${result}: Player wins!`);
-}else if ( player == 'paper' && result == 'scissors' ){
-    console.log(`${player} < ${result}: Computer wins!`);
+else if (player == "paper" && result == "rock") {
+  console.log(`${player} > ${result}: Player wins!`);
+} else if (player == "paper" && result == "scissors") {
+  console.log(`${player} < ${result}: Computer wins!`);
 }
 // ******************************************************
-else if ( player == 'scissors' && result == 'rock' ){
-    console.log(`${player} < ${result}: Computer wins!`);
-}else if ( player == 'scissors' && result == 'paper' ){
-    console.log(`${player} > ${result}: Player wins!`);
+else if (player == "scissors" && result == "rock") {
+  console.log(`${player} < ${result}: Computer wins!`);
+} else if (player == "scissors" && result == "paper") {
+  console.log(`${player} > ${result}: Player wins!`);
 }
 // ******************************************************
-else{
-    console.log("Enter rock, paper or scissors!")
+else {
+  console.log("Enter rock, paper or scissors!");
 }
-
-
